731db18f171c70ca8f7b05a7cb0ff6370b893c1a
batacraft <lordhunter017@gmail.com>
2024-07-08 17:24:40 +0545

    fetching

--- main.js
+++ main.js
@@ -1,28 +1,63 @@
+let imagesFetch = async () => {
+    try {
+        const response = await fetch('https://shikimori.one/api/animes?page=${page}&limit=12&order=popularity');
+
+        const data = await response.json();
+        // const animeList = result.data; 
+        let animeitems = ""; 
+       
+
+        data.forEach((data) => {
+
+            animeitems += `<div class=" w-[20%] h-full " id="items">
+                  <div>
+                  <img src="https://shikimori.one/${data.image.original}" alt="" style="width: 400px; height:400px" class="object-cover object-center">
+<div class="pt-6">
+ <p id="anime_name" class="font-[600] text-xl" > ${data.name}</p>
+                       
+                  </div>
+</div>
+                       
+                </div>`
+        })
+        document.getElementById('card').innerHTML = animeitems; 
+
+        // console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+imagesFetch();
+
+
+
+
+
+
+
 // let imagesFetch = async () => {
 //     try {
 //         const response = await fetch('https://kitsu.io/api/edge/anime?filter[categories]=adventure');
-
-//         const data = await response.json();
+//         const result = await response.json();
 //         const animeList = result.data; 
-//         let animeitems = ""; 
-
+//         let animeItems = "";
 //         animeList.forEach((anime) => {
 //             const posterImage = anime.attributes.posterImage.large;
-//                     const animeId = anime.id;
-//                     const titles = anime.attributes.titles;
+//             const animeId = anime.id;
+//             const titles = anime.attributes.titles.en;
 
-//             animeitems += `<div class=" w-[20rem]" id="items">
-//                   <div>
-//                   <img src="${posterImage}" alt="" style="max-width: 100%;">
+//             animeItems += `<div class=" w-[20rem]" id="items">
+//                  <div>
+//                 <img src="${posterImage}" alt="" style="max-width: 100%;">
+                 
+//                      <p id="anime_name"> ${animeId}</p>
+//                      <p id="anime_name">  <${titles}</p>
+//                    </div>             
+//                    </div>`
+//         });
 
-//                         <p id="anime_name"> ${animeId}</p>
-//                         <p id="anime_name"> russian: <${data.titles}</p>
-//                   </div>
-//                 </div>`
-//         })
-//         document.getElementById('card').innerHTML = animeitems; 
-
-//         // console.log(data);
+//         document.getElementById('card').innerHTML = animeItems;
 //     } catch (error) {
 //         console.log(error);
 //     }
@@ -33,35 +68,3 @@
 
 
 
-
-
-
-let imagesFetch = async () => {
-    try {
-        const response = await fetch('https://kitsu.io/api/edge/anime?filter[categories]=adventure');
-        const result = await response.json();
-        const animeList = result.data; // Accessing the array of anime items
-
-        let animeItems = "";
-        animeList.forEach((anime) => {
-            const posterImage = anime.attributes.posterImage.large;
-            const animeId = anime.id;
-            const titles = anime.attributes.titles.en;
-
-            animeItems += `<div class=" w-[20rem]" id="items">
-                 <div>
-                <img src="${posterImage}" alt="" style="max-width: 100%;">
-                 
-                     <p id="anime_name"> ${animeId}</p>
-                     <p id="anime_name">  <${titles}</p>
-                   </div>             
-                   </div>`
-        });
-
-        document.getElementById('card').innerHTML = animeItems;
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-imagesFetch();
